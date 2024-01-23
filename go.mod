023b855546244f53395117d80dc1cc0acc33fae2
Zenith Zack <faldund@outlook.com>
2024-01-23 17:04:55 -0500

    feat(lexer): add token and lexer struct with some reading properties, add a few tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module a-compiler-in-go/src/7west
+
+go 1.21.6
---
+++ src/7west/lexer/lexer.go
@@ -0,0 +1,65 @@
+package lexer
+
+import (
+	"a-compiler-in-go/src/7west/src/7west/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	// if we've reached the end of the input, set ch to 0, which is the ASCII code for the "NUL" character and is a
+	// common way of saying "we don't have a value here"
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		// otherwise, set ch to the next character in the input
+		l.ch = l.input[l.readPosition]
+	}
+	// advance our position in the input string
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		// we're at the end of the input
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
