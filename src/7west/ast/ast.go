763a2d3bf88a717a918cd42e03479bc39b613a36
Zenith Zack <faldund@outlook.com>
2024-01-27 12:03:06 -0500

    feat(parser): add structs for variable, identifier, expression, program, and statement

---
+++ src/7west/ast/ast.go
@@ -0,0 +1,50 @@
+package ast
+
+import (
+	"a-compiler-in-go/src/7west/src/7west/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node of every AST our parser produces
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral returns the literal value of the token associated with this node
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type VariableStatement struct {
+	Token token.Token // the token.VARIABLE token
+	Name  *Identifier
+	Value Expression
+}
+
+func (vs *VariableStatement) statementNode()       {}
+func (vs *VariableStatement) TokenLiteral() string { return vs.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
