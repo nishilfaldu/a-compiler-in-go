536e2986f3df27e0b09617e9dbceca8a4408a4c5
Zenith Zack <faldund@outlook.com>
2024-03-27 12:43:52 -0400

    feat(compiler): add cases for ExpressionStatement, ReturnStatement, CallExpression, VariableDeclaration, and Identifier

--- src/7west/compiler/compiler.go
+++ src/7west/compiler/compiler.go
@@ -3,6 +3,7 @@ package compiler
 import (
 	"a-compiler-in-go/src/7west/src/7west/ast"
 	"a-compiler-in-go/src/7west/src/7west/object"
+	"fmt"
 )
 
 type Compiler struct {
@@ -45,6 +46,55 @@ func (c *Compiler) Compile(node ast.Node) error {
 		// as it typically contains metadata about the program.
 		// You can optionally perform any necessary validation or processing here.
 
+	case *ast.ProgramBody:
+		for _, decl := range node.Declarations {
+			err := c.Compile(decl)
+			if err != nil {
+				return err
+			}
+		}
+
+		for _, stmt := range node.Statements {
+			err := c.Compile(stmt)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.VariableDeclaration:
+		symbol := c.symbolTable.Define(node.Name.Value)
+		// err := c.Compile(node.)
+
+	case *ast.Identifier:
+		symbol, ok := c.symbolTable.Resolve(node.Value)
+		if !ok {
+			return fmt.Errorf("undefined variable %s", node.Value)
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.ReturnStatement:
+		err := c.Compile(node.ReturnValue)
+		if err != nil {
+			return err
+		}
+
+	case *ast.CallExpression:
+		err := c.Compile(node.Function)
+		if err != nil {
+			return err
+		}
+
+		for _, a := range node.Arguments {
+			err := c.Compile(a)
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
